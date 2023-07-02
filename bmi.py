75f40bcc0781f8d02bd1f8a9e4296b64df6539e9
sasakiyuya617 <sasakiyuya617@gmail.com>
2023-07-02 17:16:44 +0900

    BMI終わり

--- bmi.py
+++ bmi.py
@@ -1,14 +1,19 @@
 class BMI:
     def __init__(self, height, weight):
-       self.height = height
-       self.weight = weight
+        self.value = weight / (height ** 2)
 
-    def calculate_bmi(self):
-        return self.weight / (self.height ** 2)
+        if not (10 <= self.value <= 40):
+            raise ValueError("BMIが正常値の範囲を超えています")
+
+    def __str__(self):
+        return f"{self.value:.2f}"
+
 
 tanaka_bmi = BMI(height=1.80, weight=67.0)
 sasami_bmi = BMI(height=1.58, weight=80.0)
+bob_bmi = BMI(height=1.70, weight=75000.0)
 
-print(tanaka_bmi.calculate_bmi())
-print(sasami_bmi.calculate_bmi())
+print(tanaka_bmi)
+print(sasami_bmi)
+print(bob_bmi)
 
