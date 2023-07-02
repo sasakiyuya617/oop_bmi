4385ee138f2d8dd7dbee580fad73708e8661bd72
sasakiyuya617 <sasakiyuya617@gmail.com>
2023-07-02 16:40:56 +0900

    bmi仮作成

---
+++ bmi.py
@@ -0,0 +1,14 @@
+class BMI:
+    def __init__(self, height, weight):
+       self.height = height
+       self.weight = weight
+
+    def calculate_bmi(self):
+        return self.weight / (self.height ** 2)
+
+tanaka_bmi = BMI(height=1.80, weight=67.0)
+sasami_bmi = BMI(height=1.58, weight=80.0)
+
+print(tanaka_bmi.calculate_bmi())
+print(sasami_bmi.calculate_bmi())
+
